4f0005f1c6600df5b73979760e2ece417d260063
oksana.karachun <karachun.ok@gmail.com>
2024-05-12 12:17:42 +0200

    remove index.js

--- ts/index.js
+++
@@ -1,4 +0,0 @@
-import { Chart } from "./classes/Chart";
-const chart = new Chart('chartCanvas', 'Advanced', 'EURUSD', 1, 57674, 59113);
-document.getElementById('marketUSDJPY').addEventListener('click', () => chart.changeMarket('USDJPY'));
-document.getElementById('marketEURUSD').addEventListener('click', () => chart.changeMarket('EURUSD'));
